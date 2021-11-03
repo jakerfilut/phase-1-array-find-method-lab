4d631cd32ffabf1df140bf28f21dd4d48fcadd3c
Jake <jakerfilut@gmail.com>
2021-11-03 08:53:00 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+// const record = [
+//     { year: "2018", result: "N/A"},
+//     { year: "2017", result: "N/A"},
+//     { year: "1961", result: "N/A"},
+//     { year: "1960", result: "N/A"}
+//   ]
 
+
+
+
+
+
+function superbowlWin(array){
+    let score = array.find(record => record.result === "W");
+    if (!score){
+        return undefined;
+    }else{
+        return score.year;
+    }
+}   
